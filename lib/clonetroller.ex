aaa113366f64fcb64931c986c8211e69f1b9427b
Brett Beatty <brettbeatty@gmail.com>
2024-03-18 19:39:44 -0600

    Add optional query argument

--- lib/clonetroller.ex
+++ lib/clonetroller.ex
@@ -9,6 +9,10 @@ defmodule Clonetroller do
   @callback request(verb(), path(), request(), meta()) :: any()
 
   @typep action() :: atom()
+  @typep arity_group() ::
+           {[atom()], endpoint(), endpoint()}
+           | {[atom()], nil, endpoint()}
+           | {[atom()], endpoint(), nil}
   @typep endpoint() :: %{
            action: action(),
            controller: controller(),
@@ -17,7 +21,6 @@ defmodule Clonetroller do
            path_params: [String.t()],
            verb: verb()
          }
-  @typep variable() :: {atom(), keyword(), atom()}
 
   defmacro clone(router, controller) do
     router = Macro.expand(router, __CALLER__)
@@ -29,7 +32,7 @@ defmodule Clonetroller do
   defp build_clone(router, controller) do
     controller
     |> get_endpoints(router)
-    |> uniq()
+    |> build_arity_groups()
     |> build_functions(router)
   end
 
@@ -53,49 +56,104 @@ defmodule Clonetroller do
     for ":" <> param <- Path.split(path), do: param
   end
 
-  @spec uniq([endpoint()]) :: [endpoint()]
-  defp uniq(endpoints) do
+  @spec build_arity_groups([endpoint()]) :: [arity_group()]
+  defp build_arity_groups(endpoints) do
     endpoints
-    |> Enum.group_by(&{&1.action, length(&1.path_params)})
-    |> Enum.map(fn {_key, group} -> hd(group) end)
+    |> group_by_action_and_arity()
+    |> merge_args()
   end
 
-  @spec build_functions([endpoint()], router()) :: Macro.t()
-  defp build_functions(endpoints, router) do
-    quote generated: true do
-      (unquote_splicing(
-         for endpoint <- endpoints do
-           vars = build_vars(endpoint)
+  @spec group_by_action_and_arity([endpoint()]) :: %{
+          {action(), arity()} => {endpoint() | nil, endpoint() | nil}
+        }
+  defp group_by_action_and_arity(endpoints) do
+    Enum.reduce(endpoints, %{}, fn endpoint, acc ->
+      action = endpoint.action
+      arity = length(endpoint.path_params)
 
-           quote generated: true do
-             def unquote(endpoint.action)(unquote_splicing(vars), request) do
-               request(
-                 unquote(endpoint.verb),
-                 unquote(router).Helpers.unquote(:"#{endpoint.helper}_path")(
-                   %URI{},
-                   unquote(endpoint.action),
-                   unquote_splicing(vars)
-                 ),
-                 request,
-                 %{
-                   controller: unquote(endpoint.controller),
-                   path: unquote(endpoint.path),
-                   path_params: unquote(Enum.map(vars, &{elem(&1, 0), &1}))
-                 }
-               )
-             end
-           end
-         end
-       ))
+      acc
+      |> Map.update({action, arity + 1}, {nil, endpoint}, &put_elem(&1, 1, endpoint))
+      |> Map.update({action, arity + 2}, {endpoint, nil}, &put_elem(&1, 0, endpoint))
+    end)
+  end
+
+  @spec merge_args(%{{action(), arity()} => {endpoint() | nil, endpoint() | nil}}) ::
+          [arity_group()]
+  defp merge_args(groups) do
+    Enum.map(groups, fn {{_action, _arity}, {query_endpoint, no_query_endpoint}} ->
+      query_args = get_args(query_endpoint, ["request", "query"])
+      no_query_args = get_args(no_query_endpoint, ["request"])
+      args = [no_query_args, query_args] |> Enum.reject(&is_nil/1) |> Enum.zip_with(&merge_arg/1)
+      {args, query_endpoint, no_query_endpoint}
+    end)
+  end
+
+  @spec get_args(endpoint() | nil, [String.t()]) :: [String.t()] | nil
+  defp get_args(endpoint, suffix) do
+    if endpoint do
+      endpoint.path_params ++ suffix
     end
   end
 
-  @spec build_vars(endpoint()) :: [variable()]
-  defp build_vars(endpoint) do
-    for param <- endpoint.path_params do
-      param
-      |> String.to_atom()
-      |> Macro.unique_var(__MODULE__)
+  @spec merge_arg([String.t()]) :: atom()
+  defp merge_arg(names) do
+    names
+    |> Enum.uniq()
+    |> Enum.join("_or_")
+    |> String.to_atom()
+  end
+
+  @spec build_functions([arity_group()], router()) :: Macro.t()
+  defp build_functions(endpoints, router) do
+    functions =
+      Enum.flat_map(endpoints, fn {vars, query_endpoint, no_query_endpoint} ->
+        [
+          query_endpoint && build_function(vars, -2, query_endpoint, router),
+          no_query_endpoint && build_function(vars, -1, no_query_endpoint, router)
+        ]
+      end)
+
+    {:__block__, [generated: true], Enum.reject(functions, &is_nil/1)}
+  end
+
+  @spec build_function([atom()], -2..-1, endpoint(), router()) :: Macro.t()
+  defp build_function(args, request_index, endpoint, router) do
+    action = endpoint.action
+    helper = :"#{endpoint.helper}_path"
+    verb = endpoint.verb
+
+    vars = Enum.map(args, &Macro.var(&1, __MODULE__))
+    {request_var, vars_without_request} = List.pop_at(vars, request_index)
+
+    path_params = endpoint.path_params |> Enum.map(&String.to_atom/1) |> Enum.zip(vars)
+
+    path =
+      quote generated: true do
+        unquote(router).Helpers.unquote(helper)(
+          %URI{},
+          unquote(action),
+          unquote_splicing(vars_without_request)
+        )
+      end
+
+    meta =
+      quote generated: true do
+        %{
+          controller: unquote(endpoint.controller),
+          path: unquote(endpoint.path),
+          path_params: unquote(path_params)
+        }
+      end
+
+    quote generated: true do
+      def unquote(action)(unquote_splicing(vars)) when is_map(unquote(request_var)) do
+        request(
+          unquote(verb),
+          unquote(path),
+          unquote(request_var),
+          unquote(meta)
+        )
+      end
     end
   end
 end
--- test/clonetroller_test.exs
+++ test/clonetroller_test.exs
@@ -9,7 +9,7 @@ defmodule ClonetrollerTest do
 
     resources "/resources", ResourceController
 
-    scope "/admin" do
+    scope "/parents/:parent_id" do
       resources "/resources", ResourceController
     end
   end
@@ -29,6 +29,8 @@ defmodule ClonetrollerTest do
   setup do
     [req: %{ref: make_ref()}]
   end
+
+  defp generate_id, do: 3 |> :crypto.strong_rand_bytes() |> Base.url_encode64()
 
   describe "basic clone" do
     test "index", %{req: req} do
@@ -50,7 +52,7 @@ defmodule ClonetrollerTest do
     end
 
     test "show", %{req: req} do
-      id = 3 |> :crypto.strong_rand_bytes() |> Base.url_encode64()
+      id = generate_id()
 
       assert {:get, path, ^req, meta} = Resource.show(id, req)
       assert path == "/resources/#{id}"
@@ -63,7 +65,7 @@ defmodule ClonetrollerTest do
     end
 
     test "edit", %{req: req} do
-      id = 3 |> :crypto.strong_rand_bytes() |> Base.url_encode64()
+      id = generate_id()
 
       assert {:get, path, ^req, meta} = Resource.edit(id, req)
       assert path == "/resources/#{id}/edit"
@@ -76,7 +78,7 @@ defmodule ClonetrollerTest do
     end
 
     test "update", %{req: req} do
-      id = 3 |> :crypto.strong_rand_bytes() |> Base.url_encode64()
+      id = generate_id()
 
       assert {:patch, path, ^req, meta} = Resource.update(id, req)
       assert path == "/resources/#{id}"
@@ -89,7 +91,7 @@ defmodule ClonetrollerTest do
     end
 
     test "delete", %{req: req} do
-      id = 3 |> :crypto.strong_rand_bytes() |> Base.url_encode64()
+      id = generate_id()
 
       assert {:delete, path, ^req, meta} = Resource.delete(id, req)
       assert path == "/resources/#{id}"
@@ -98,6 +100,287 @@ defmodule ClonetrollerTest do
                controller: ResourceController,
                path: "/resources/:id",
                path_params: [id: id]
+             }
+    end
+  end
+
+  describe "duplicate routes with additional path params" do
+    test "index w/ parent id", %{req: req} do
+      parent_id = generate_id()
+
+      assert {:get, path, ^req, meta} = Resource.index(parent_id, req)
+      assert path == "/parents/#{parent_id}/resources"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources",
+               path_params: [parent_id: parent_id]
+             }
+    end
+
+    test "index w/ query", %{req: req} do
+      page_size = :rand.uniform(1_000)
+      cursor = generate_id()
+
+      assert {:get, path, ^req, meta} = Resource.index(req, after: cursor, page_size: page_size)
+      assert path == "/resources?after=#{cursor}&page_size=#{page_size}"
+
+      assert meta == %{controller: ResourceController, path: "/resources", path_params: []}
+    end
+
+    test "index w/ parent id and query", %{req: req} do
+      parent_id = generate_id()
+      page_size = :rand.uniform(1_000)
+      cursor = generate_id()
+
+      assert {:get, path, ^req, meta} =
+               Resource.index(parent_id, req, after: cursor, page_size: page_size)
+
+      assert path == "/parents/#{parent_id}/resources?after=#{cursor}&page_size=#{page_size}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources",
+               path_params: [parent_id: parent_id]
+             }
+    end
+
+    test "new w/ parent id", %{req: req} do
+      parent_id = generate_id()
+
+      assert {:get, path, ^req, meta} = Resource.new(parent_id, req)
+      assert path == "/parents/#{parent_id}/resources/new"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/new",
+               path_params: [parent_id: parent_id]
+             }
+    end
+
+    test "new w/ query", %{req: req} do
+      redirect_uri = "https://example.com/#{generate_id()}"
+
+      assert {:get, path, ^req, meta} = Resource.new(req, redirect_uri: redirect_uri)
+      assert path == "/resources/new?redirect_uri=#{URI.encode_www_form(redirect_uri)}"
+      assert meta == %{controller: ResourceController, path: "/resources/new", path_params: []}
+    end
+
+    test "new w/ parent id and query", %{req: req} do
+      parent_id = generate_id()
+      redirect_uri = "https://example.com/#{generate_id()}"
+
+      assert {:get, path, ^req, meta} = Resource.new(parent_id, req, redirect_uri: redirect_uri)
+
+      assert path ==
+               "/parents/#{parent_id}/resources/new?redirect_uri=#{URI.encode_www_form(redirect_uri)}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/new",
+               path_params: [parent_id: parent_id]
+             }
+    end
+
+    test "create w/ parent_id", %{req: req} do
+      parent_id = generate_id()
+
+      assert {:post, path, ^req, meta} = Resource.create(parent_id, req)
+      assert path == "/parents/#{parent_id}/resources"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources",
+               path_params: [parent_id: parent_id]
+             }
+    end
+
+    test "create w/ query", %{req: req} do
+      assert {:post, path, ^req, meta} = Resource.create(req, quiet: true)
+      assert path == "/resources?quiet=true"
+      assert meta == %{controller: ResourceController, path: "/resources", path_params: []}
+    end
+
+    test "create w/ parent_id and query", %{req: req} do
+      parent_id = generate_id()
+
+      assert {:post, path, ^req, meta} = Resource.create(parent_id, req, quiet: true)
+      assert path == "/parents/#{parent_id}/resources?quiet=true"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources",
+               path_params: [parent_id: parent_id]
+             }
+    end
+
+    test "show w/ parent id", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+
+      assert {:get, path, ^req, meta} = Resource.show(parent_id, id, req)
+      assert path == "/parents/#{parent_id}/resources/#{id}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "show w/ query", %{req: req} do
+      id = generate_id()
+      query = [format: :long, show_banner: true]
+
+      assert {:get, path, ^req, meta} = Resource.show(id, req, query)
+      assert path == "/resources/#{id}?format=long&show_banner=true"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/resources/:id",
+               path_params: [id: id]
+             }
+    end
+
+    test "show w/ parent id and query", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+      query = [format: :long, show_banner: true]
+
+      assert {:get, path, ^req, meta} = Resource.show(parent_id, id, req, query)
+      assert path == "/parents/#{parent_id}/resources/#{id}?format=long&show_banner=true"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "edit w/ parent id", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+
+      assert {:get, path, ^req, meta} = Resource.edit(parent_id, id, req)
+      assert path == "/parents/#{parent_id}/resources/#{id}/edit"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id/edit",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "edit w/ query", %{req: req} do
+      id = generate_id()
+      query = [suggestions: "off"]
+
+      assert {:get, path, ^req, meta} = Resource.edit(id, req, query)
+      assert path == "/resources/#{id}/edit?suggestions=off"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/resources/:id/edit",
+               path_params: [id: id]
+             }
+    end
+
+    test "edit w/ parent id and query", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+      query = [suggestions: "off"]
+
+      assert {:get, path, ^req, meta} = Resource.edit(parent_id, id, req, query)
+      assert path == "/parents/#{parent_id}/resources/#{id}/edit?suggestions=off"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id/edit",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "update w/ parent id", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+
+      assert {:patch, path, ^req, meta} = Resource.update(parent_id, id, req)
+      assert path == "/parents/#{parent_id}/resources/#{id}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "update w/ query", %{req: req} do
+      id = generate_id()
+
+      assert {:patch, path, ^req, meta} = Resource.update(id, req, [])
+      assert path == "/resources/#{id}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/resources/:id",
+               path_params: [id: id]
+             }
+    end
+
+    test "update w/ parent id and query", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+
+      assert {:patch, path, ^req, meta} = Resource.update(parent_id, id, req, [])
+      assert path == "/parents/#{parent_id}/resources/#{id}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "delete w/ parent id", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+
+      assert {:delete, path, ^req, meta} = Resource.delete(parent_id, id, req)
+      assert path == "/parents/#{parent_id}/resources/#{id}"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id",
+               path_params: [parent_id: parent_id, id: id]
+             }
+    end
+
+    test "delete w/ query", %{req: req} do
+      id = generate_id()
+      query = [type: :soft, notify: true]
+
+      assert {:delete, path, ^req, meta} = Resource.delete(id, req, query)
+      assert path == "/resources/#{id}?type=soft&notify=true"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/resources/:id",
+               path_params: [id: id]
+             }
+    end
+
+    test "delete w/ parent id and query", %{req: req} do
+      parent_id = generate_id()
+      id = generate_id()
+      query = [type: :soft, notify: true]
+
+      assert {:delete, path, ^req, meta} = Resource.delete(parent_id, id, req, query)
+      assert path == "/parents/#{parent_id}/resources/#{id}?type=soft&notify=true"
+
+      assert meta == %{
+               controller: ResourceController,
+               path: "/parents/:parent_id/resources/:id",
+               path_params: [parent_id: parent_id, id: id]
              }
     end
   end
